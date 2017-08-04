7c5b0afa992a52defe52230c45fa98b29b81b887
homac <homac@strace.org>
2017-08-04 14:31:08 +0000

    + mc

--- src/org/cakelab/glsl/pp/ast/MacroCallExpression.java
+++ src/org/cakelab/glsl/pp/ast/MacroCallExpression.java
@@ -1,6 +1,5 @@
 package org.cakelab.glsl.pp.ast;
 
-import org.cakelab.glsl.Interval;
 import org.cakelab.glsl.Location;
 import org.cakelab.glsl.lang.EvaluationException;
 import org.cakelab.glsl.lang.ast.CallExpression;
